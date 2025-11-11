6e8ff870e8acca957170fcbeb4fda8d849d34daa
rifatNurAl <2306853@edu.karelia.fi>
2025-11-11 16:31:35 +0600

    added post functionality

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const app = express();
 
 app.use(express.json());
 
+// Sample dataset
+let data = [
+  { id: 1, firstname: "Foysal", lastname: "Rifat" },
+  { id: 2, firstname: "Ariyan", lastname: "Fahad" },
+];
+
+// =============================
+// POST /data 
+app.post("/data", (req, res) => {
+  if (!req.is("application/json")) {
+    return res.status(415).json({ error: "Unsupported Media Type" });
+  }
+
+  const newItem = req.body;
+  data.push(newItem);
+  res.status(201).json(newItem);
+});
 
 
 // ====================================
